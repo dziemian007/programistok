3933d7fae2f50781498f733e44c28cb11fc26a62
dziemian007 <dziemian_rec@wp.pl>
2012-01-03 12:11:04 +0100

    remove migration

--- db/migrate/20120102110051_change_presentation_in_topics.rb
+++ db/migrate/20120102110051_change_presentation_in_topics.rb
@@ -1,10 +1,2 @@
 class ChangePresentationInTopics < ActiveRecord::Migration
-  def self.up
-    change_table :topics do |t|
-      t.change :presentation, :boolean
-    end
-  end
-
-  def down
-  end
 end
