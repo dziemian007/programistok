a6826947073e4f981d6a983207c872bf05f1527d
dziemian007 <dziemian_rec@wp.pl>
2012-01-03 12:29:16 +0100

    change migration to topics table

--- db/migrate/20120102103954_add_presentation_to_topics.rb
+++ db/migrate/20120102103954_add_presentation_to_topics.rb
@@ -1,6 +1,6 @@
 class AddPresentationToTopics < ActiveRecord::Migration
   def change
-    add_column :topics, :presentation, :integer
+    add_column :topics, :presentation, :boolean
     remove_column :topics, :proposal
   end
 end
